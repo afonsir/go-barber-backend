1939323132eadd67977c1a8b0b0a4268ce28b843
afonsir17 <afonsir17@gmail.com>
2020-12-29 10:52:22 -0300

    Parsing date with date-fns

--- src/routes/appoitments.routes.ts
+++ src/routes/appoitments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appoitmentsRouter = Router();
 
@@ -8,10 +9,12 @@ const appointments = [];
 appoitmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
   const appointment = {
     id: uuid(),
     provider,
-    date
+    date: parsedDate
   };
 
   appointments.push(appointment);
