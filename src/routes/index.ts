38e4573bb2a234870ea4d07184306ae9d90bdbf3
afonsir17 <afonsir17@gmail.com>
2020-12-29 10:23:25 -0300

    Adding Appointments router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import appoitmentsRouter from './appoitments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', appoitmentsRouter);
 
 export default routes;
