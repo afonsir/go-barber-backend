5ee36d046049f683716404f0f72d21558cc0b76d
afonsir17 <afonsir17@gmail.com>
2020-12-29 11:40:43 -0300

    Refactoring: sorting methods by name

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -9,20 +9,20 @@ class AppointmentsRepository {
     this.appointments = [];
   }
 
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date)
-    );
-
-    return findAppointment || null;
-  }
-
   public create(provider: string, date: Date): Appointment {
     const appointment = new Appointment(provider, date);
 
     this.appointments.push(appointment);
 
     return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date)
+    );
+
+    return findAppointment || null;
   }
 }
 
