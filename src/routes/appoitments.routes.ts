4b4d5ba9dac2a4ef66dfb82a8319c4bcf1eb79bf
afonsir17 <afonsir17@gmail.com>
2020-12-29 10:52:39 -0300

    Defining Appointment interface

--- src/routes/appoitments.routes.ts
+++ src/routes/appoitments.routes.ts
@@ -4,12 +4,28 @@ import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appoitmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+};
+
+const appointments: Appointment[] = [];
 
 appoitmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
