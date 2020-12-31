07cd1245ba36b2506bfa116eb84840d1c69c600b
afonsir17 <afonsir17@gmail.com>
2020-12-31 10:53:38 -0300

    Adding User model

---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+export default User;
