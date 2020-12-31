4fa55fc199e4571b931c665cebc1c41ae16b81f9
afonsir17 <afonsir17@gmail.com>
2020-12-31 16:03:37 -0300

    Adding User router and service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appoitments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
